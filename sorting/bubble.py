f2227af4a3e64469cfa2f0b5373d1b16b0db49e7
Irfan Radzi <rolodex888@gmail.com>
2018-10-26 08:59:27 +0800

    Added check for numbers, throwing it properly.

--- sorting/bubble.py
+++ sorting/bubble.py
@@ -1,11 +1,14 @@
 def bubble(numbers):
-    n = len(numbers)
-    # traverse through all elements in the array
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if numbers[j] > numbers[j+1]:
-                # TODO: swap numbers if element is greater
-                temp = numbers[j]
-                numbers[j] = numbers[j + 1]
-                numbers[j + 1] = temp
-            return numbers;
+    try:
+        n = len(numbers)
+        # traverse through all elements in the array
+        for i in range(n):
+            for j in range(0, n-i-1):
+                if numbers[j] > numbers[j+1]:
+                    # TODO: swap numbers if element is greater
+                    temp = numbers[j]
+                    numbers[j] = numbers[j + 1]
+                    numbers[j + 1] = temp
+                return numbers;
+    except TypeError as error:
+        print "Params passed must be a list type"
